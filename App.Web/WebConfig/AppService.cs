06b1d366eecd000d783f4ce4149348c36e332db7
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-14 13:30:38 +0700

    Create AppService.cs

---
+++ App.Web/WebConfig/AppService.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Web.WebConfig
+{
+	public static class AppService
+	{
+		public static string WebRootPath { get; private set; }
+		public static IConfiguration Configuration { get; }
+
+		public static void AddAppService(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
+		{
+			WebRootPath = env.WebRootPath;
+		}
+
+	}
+}
