c865eccd9ab8433818c2dbd3e78ad211c81f8997
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-14 13:31:43 +0700

    Update AppClaimTypes.cs

--- App.Web/WebConfig/Consts/AppClaimTypes.cs
+++ App.Web/WebConfig/Consts/AppClaimTypes.cs
@@ -3,9 +3,10 @@
 	public static class AppClaimTypes
 	{
 		public const string Permissions = "USER_PERMISSIONS";
-		public const string FullName = "USER_FULLNAME";
+        public const string Avatar		= "USER_AVATAR";
+		public const string FullName	= "USER_FULLNAME";
 		public const string PhoneNumber = "USER_PHONE_NUMBER";
-		public const string RoleName = "USER_ROLE_NAME";
-		public const string RoleId = "USER_ROLE_ID";
+		public const string RoleName	= "USER_ROLE_NAME";
+		public const string RoleId		= "USER_ROLE_ID";
 	}
 }
