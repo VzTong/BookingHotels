f3535cf3ec707c4c5f4ef622554ed8123278599b
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:10:44 +0700

    Create AcceptUpdateViewModel.cs

---
+++ App.Web/Areas/Admin/ViewModels/Account/AcceptUpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Account
+{
+	public class AcceptUpdateViewModel
+	{
+		public int Id { get; set; }
+		public string FullName { get; set; }
+		public string PhoneNumber1 { get; set; }
+		public string PhoneNumber2 { get; set; }
+		public string Avatar { get; set; }
+		public string Address { get; set; }
+	}
+}
