847b8dbc7a07e83d23f539e946ea67102fcb02d2
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:04:24 +0700

    Create ChangePwdVM.cs

---
+++ App.Web/ViewModels/Account/ChangePwdVM.cs
@@ -0,0 +1,25 @@
+﻿using App.Share.Attributes;
+using App.Web.WebConfig.Consts;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Web.ViewModels.Account
+{
+	public class ChangePwdVM
+	{
+
+		[DataType(DataType.Password)]
+		[AppRequired]
+		public string Pwd { get; set; }
+
+		[DataType(DataType.Password)]
+		[AppRequired]
+		[AppMinLength(VM.UserVM.PWD_MINLEN)]
+		public string NewPwd { get; set; }
+
+		[DataType(DataType.Password)]
+		[AppConfirmPwd("NewPwd")]
+		public string ConfirmPassword { get; set; }
+
+		public bool LogoutAfterChangePwd { get; set; }
+	}
+}
