d98e41b92de20180f41fc4eefcde23d865a95128
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:05:46 +0700

    Create UserProfileClientVM.cs

---
+++ App.Web/ViewModels/Account/UserProfileClientVM.cs
@@ -0,0 +1,20 @@
+﻿using App.Share.Attributes;
+
+namespace App.Web.ViewModels.Account
+{
+	public class UserProfileClientVM
+	{
+		[AppRequired]
+		public string FullName { get; set; }
+		[AppRequired]
+		[AppPhone]
+		public string PhoneNumber1 { get; set; }
+		[AppPhone]
+		public string PhoneNumber2 { get; set; }
+		[AppRequired]
+		[AppEmail]
+		public string Email { get; set; }
+		[AppRequired]
+		public string Address { get; set; }
+	}
+}
