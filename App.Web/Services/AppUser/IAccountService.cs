c01c9ded887f121eb730ac3d5d2b68509c17eb9d
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:03:53 +0700

    Create IAccountService.cs

---
+++ App.Web/Services/AppUser/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using App.Web.Areas.Admin.ViewModels.Account;
+
+namespace App.Web.Services.AppUser
+{
+	public interface IAccountService
+	{
+		Task<UpdateUserViewModel> GetUserById(int? id);
+		Task UpdateUser(AcceptUpdateViewModel data);
+	}
+}
