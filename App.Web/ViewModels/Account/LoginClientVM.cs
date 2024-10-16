f485ace6e79e4a1cf44909e260bbdca3ff086ff3
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:05:04 +0700

    Create LoginClientVM.cs

---
+++ App.Web/ViewModels/Account/LoginClientVM.cs
@@ -0,0 +1,15 @@
+﻿using App.Share.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Web.ViewModels.Account
+{
+	public class LoginClientVM
+	{
+		[AppRequired]
+		public string Username { get; set; }
+
+		[DataType(DataType.Password)]
+		[AppRequired]
+		public string Password { get; set; }
+	}
+}
