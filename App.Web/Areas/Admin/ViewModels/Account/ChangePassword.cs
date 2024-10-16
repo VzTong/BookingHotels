8824ef3773d864d9804435f0ef17d90225cec4fe
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:10:56 +0700

    Create ChangePassword.cs

---
+++ App.Web/Areas/Admin/ViewModels/Account/ChangePassword.cs
@@ -0,0 +1,25 @@
+﻿using App.Share.Attributes;
+using App.Web.WebConfig.Consts;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Web.Areas.Admin.ViewModels.Account
+{
+	public class ChangePassword
+	{
+		[DataType(DataType.Password)]
+		[AppRequired]
+		public string Pwd { get; set; }
+
+		[DataType(DataType.Password)]
+		[AppRequired]
+		[AppMinLength(VM.UserVM.PWD_MINLEN)]
+		public string NewPwd { get; set; }
+
+		[DataType(DataType.Password)]
+        [AppRequired]
+        [AppConfirmPwd("NewPwd")]
+		public string ConfirmPassword { get; set; }
+
+		public bool LogoutAfterChangePwd { get; set; }
+	}
+}
