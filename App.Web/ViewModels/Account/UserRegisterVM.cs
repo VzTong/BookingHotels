94487a2cf7f963caf3d2eaac909bdb370f5b6a82
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-16 16:06:31 +0700

    Create UserRegisterVM.cs

---
+++ App.Web/ViewModels/Account/UserRegisterVM.cs
@@ -0,0 +1,46 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+using App.Web.WebConfig.Consts;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Web.ViewModels.Account
+{
+	public class UserRegisterVM
+	{
+		[AppRequired]
+		[AppUsername]
+		[AppStringLength(VM.UserVM.USERNAME_MINLEN, DB.AppUser.USERNAME_LENGTH)]
+		public string Username { get; set; }
+
+		[AppRequired]
+		[DataType(DataType.Password)]
+		[AppStringLength(VM.UserVM.PWD_MINLEN, DB.AppUser.PWD_LENGTH)]
+		public string Password { get; set; }
+
+		[AppRequired]
+		[AppConfirmPwd]
+		[DataType(DataType.Password)]
+		public string ConfirmPwd { get; set; }
+
+		[AppRequired]
+		public string FullName { get; set; }
+
+		[AppRequired]
+		[AppPhone]
+		public string PhoneNumber1 { get; set; }
+
+		[AppPhone]
+		public string PhoneNumber2 { get; set; }
+
+		[AppRequired]
+		[AppEmail]
+		public string Email { get; set; }
+		public string Address { get; set; }
+
+		public byte[] PasswordHash { get; internal set; }
+		public byte[] PasswordSalt { get; internal set; }
+
+		[AppRequired]
+		public bool IsSubmit { get; set; }
+	}
+}
