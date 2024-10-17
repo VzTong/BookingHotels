20635da6e44b345593f0c96a4386f8d8549ac1b1
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-17 19:14:19 +0700

    Create AppRoomEquipment.cs

---
+++ App.Data/Entities/Room/AppRoomEquipment.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Data.Entities.Room
+{
+    public class AppRoomEquipment
+    {
+        public int Id { get; set; }
+        public int? RoomId { get; set; }
+        public int? EquipmentId { get; set; }
+
+        public AppRoom Room { get; set; }
+        public AppEquipment Equipment { get; set; }
+    }
+}
