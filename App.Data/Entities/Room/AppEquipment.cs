f40c0f8d7427b8fac93096509f7c05fe099f27f9
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-17 19:15:51 +0700

    Update AppEquipment.cs

--- App.Data/Entities/Room/AppEquipment.cs
+++ App.Data/Entities/Room/AppEquipment.cs
@@ -6,13 +6,13 @@ namespace App.Data.Entities.Room
     {
         public AppEquipment()
         {
-            Rooms = new HashSet<AppRoom>();
+            RoomEquipments = new HashSet<AppRoomEquipment>();
         }
         public string Name { get; set; }
         public string? Description { get; set; }
         public int? TypeEquipmentId { get; set; }
 
-        public AppTypeEquipment? TypeEquipment { get; set; }
-        public ICollection<AppRoom> Rooms { get; set; }
+        public AppEquipmentType? TypeEquipment { get; set; }
+        public ICollection<AppRoomEquipment> RoomEquipments { get; set; }
     }
 }
