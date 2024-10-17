e70b6e3d7f2fa10d056d79f324fae3449c990fbb
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-17 19:11:31 +0700

    Update AppNewsCategoryConfig

---
+++ App.Data/Configurations/WebService/AppNewsCategoryConfig.cs
@@ -0,0 +1,32 @@
+﻿using App.Data.Entities.News;
+using App.Share.Consts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace App.Data.Configurations.WebService
+{
+	public class AppNewsCategoryConfig : IEntityTypeConfiguration<AppNewsCategories>
+	{
+		public void Configure(EntityTypeBuilder<AppNewsCategories> builder)
+		{
+			builder.ToTable(DB.AppNewsCategory.TABLE_NAME);
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Title)
+				.IsRequired()
+				.HasMaxLength(DB.AppNewsCategory.TITLE_LENGTH);
+
+			builder.Property(x => x.Slug)
+				.HasMaxLength(DB.AppNewsCategory.SLUG_LENGTH);
+
+			builder.Property(x => x.Content)
+				.HasMaxLength(DB.AppNewsCategory.MAX_LENGTH);
+
+			builder.Property(x => x.CoverImgPath)
+				.HasMaxLength(DB.AppNewsCategory.COVER_IMG_LENGTH);
+
+			builder.Property(x => x.CreatedDate)
+				.HasDefaultValueSql(DB.AppNewsCategory.DEFAULT_DATE);
+		}
+	}
+}
