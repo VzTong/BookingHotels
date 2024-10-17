29a12dcbd833bdfd6eef1ac203aa75b503ab3741
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-17 19:14:36 +0700

    Update AppNews.cs

--- App.Data/Entities/WebService/AppNews.cs
+++ App.Data/Entities/WebService/AppNews.cs
@@ -20,6 +20,6 @@ namespace App.Data.Entities.News
         public int? CategoryId { get; set; }
 
         public AppUser Users { get; set; }
-        public AppCategoryNews CategoryNews { get; set; }
+        public AppNewsCategory CategoryNews { get; set; }
     }
 }
