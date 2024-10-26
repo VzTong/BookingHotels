53a822646bbb09f25e7068af31ac5568701320a9
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-26 19:59:24 +0700

    Create AppBranchHotelListItemVM.cs

---
+++ App.Web/Areas/Admin/ViewModels/BranchHotel/AppBranchHotelListItemVM.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.BranchHotel
+{
+    public class AppBranchHotelListItemVM : ListItemBaseVM
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string Address { get; set; }
+        public int? QuantityStaff { get; set; }
+        public int? QuantityRoom { get; set; }
+        public string? Img { get; set; }
+	}
+}
