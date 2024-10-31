ac24e5682315348624f8cd85f7fe1a3010812198
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-31 21:30:48 +0700

    Create View Equipment - Admin

---
+++ App.Web/Areas/Admin/ViewModels/Equipment/AddOrUpdateEquipmentVM.cs
@@ -0,0 +1,18 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+using App.Web.WebConfig.Consts;
+
+namespace App.Web.Areas.Admin.ViewModels.Equipment
+{
+    public class AddOrUpdateEquipmentVM
+    {
+        public int? Id { get; set; }
+
+        [AppRequired]
+        [AppStringLength(VM.ProductCategoryVM.MIN_LENGTH, DB.AppEquipment.NAME_LENGTH)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+        public int? TypeEquipmentId { get; set; }
+    }
+}
---
+++ App.Web/Areas/Admin/ViewModels/Equipment/EquipmentListItemVM.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Equipment
+{
+	public class EquipmentListItemVM : ListItemBaseVM
+	{
+		public string Name { get; set; }
+		public string? Description { get; set; }
+		public int? TypeEquipmentId { get; set; }
+		public string? TypeEquipmentName { get; set; }
+	}
+}
