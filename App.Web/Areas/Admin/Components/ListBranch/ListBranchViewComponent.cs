6454f28523b8476958516cb9f42372c3fb77f0e9
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-31 21:17:59 +0700

    Create components ListBranch

---
+++ App.Web/Areas/Admin/Components/ListBranch/ListBranchViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using App.Data.Entities.Hotel;
+using App.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Areas.Admin.Components.ListBranchHotel
+{
+	public class ListBranchViewComponent : ViewComponent
+	{
+		readonly GenericRepository repository;
+		public ListBranchViewComponent(GenericRepository _db)
+		{
+			repository = _db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
+		{
+			var data = await repository
+					.GetAll<AppBranchHotel>(where: r => excludeIds == null || excludeIds.Any() && !excludeIds.Contains(r.Id))
+					.ToListAsync();
+			ViewBag.SelectedId = seletetedId;
+			return View(data);
+		}
+	}
+}
