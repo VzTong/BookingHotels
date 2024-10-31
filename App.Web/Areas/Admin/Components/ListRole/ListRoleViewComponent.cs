54010ed881dab84d7a3a4fc034fe6692fdfb7679
Vz <119030528+VzTong@users.noreply.github.com>
2024-10-31 21:20:41 +0700

    Create Components ListRole

---
+++ App.Web/Areas/Admin/Components/ListRole/ListRoleViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using App.Data.Entities.User;
+using App.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Areas.Admin.Components.ListRole
+{
+	public class ListRoleViewComponent : ViewComponent
+	{
+		readonly GenericRepository repository;
+		public ListRoleViewComponent(GenericRepository _db)
+		{
+			repository = _db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
+		{
+			var data = await repository
+					.GetAll<AppRole>(where: r => excludeIds == null || excludeIds.Any() && !excludeIds.Contains(r.Id))
+					.ToListAsync();
+			ViewBag.SelectedId = seletetedId;
+			return View(data);
+		}
+	}
+}
