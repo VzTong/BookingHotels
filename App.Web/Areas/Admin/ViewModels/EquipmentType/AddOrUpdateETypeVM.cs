068a862db665d91b4f01281cb02e4c642ad81132
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-13 00:08:43 +0700

    Create EType VM

---
+++ App.Web/Areas/Admin/ViewModels/EquipmentType/AddOrUpdateETypeVM.cs
@@ -0,0 +1,15 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+using App.Web.WebConfig.Consts;
+
+namespace App.Web.Areas.Admin.ViewModels.EquipmentType
+{
+	public class AddOrUpdateETypeVM
+	{
+		public int? Id { get; set; }
+
+		[AppRequired]
+		[AppStringLength(VM.ProductCategoryVM.MIN_LENGTH, DB.AppEquipmentType.NAME_LENGTH)]
+		public string Name { get; set; }
+	}
+}
---
+++ App.Web/Areas/Admin/ViewModels/EquipmentType/ETypeListItemVM.cs
@@ -0,0 +1,11 @@
+﻿using App.Data.Entities.Room;
+
+namespace App.Web.Areas.Admin.ViewModels.EquipmentType
+{
+	public class ETypeListItemVM : ListItemBaseVM
+	{
+		public string Name { get; set; }
+		public List<string> EquipmentName { get; set; }
+		public ICollection<AppEquipment> AppEquipments { get; set; }
+	}
+}
