cb08c12aa1f966019a06a00d6e894087b262ebaf
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-13 00:09:52 +0700

    Create Etype js

---
+++ App.Web/wwwroot/js/Admin/EType/eType-modal.js
@@ -0,0 +1,10 @@
+﻿function UpdateEType(id, name) {
+	// Đặt các giá trị trong modal cập nhật
+	$('#updateEType #idEdit').val(id);
+	$('#updateEType #eTypeName-field').val(name);
+}
+
+function setDeleteId(id) {
+	const deleteConfirmBtn = document.getElementById('deleteConfirmBtn');
+	deleteConfirmBtn.href = `/Admin/AppEType/Delete/${id}`;
+}
---
+++ App.Web/wwwroot/js/Admin/EType/eType-validate.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    // Equipment type name validation
+    $('#eTypeName-field').on('input', function () {
+        var name = $(this);
+        var value = name.val().trim();
+        var isValid = value.length >= 1 && value.length <= 200;
+
+        if (isValid) {
+            name[0].setCustomValidity('');
+            name.removeClass('is-invalid').addClass('is-valid');
+        } else {
+            name[0].setCustomValidity('Tên loại thiết bị không hợp lệ');
+            name.removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+});
