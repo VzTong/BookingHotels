383cb04bd47cbbd723713266823b21798bfb32f2
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-15 22:36:29 +0700

    Create CateNews VM

---
+++ App.Web/Areas/Admin/ViewModels/CategoryNews/AddOrUpdateCategoryNewsVM.cs
@@ -0,0 +1,20 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+using App.Web.WebConfig.Consts;
+
+namespace App.Web.Areas.Admin.ViewModels.CategoryNews
+{
+	public class AddOrUpdateCategoryNewsVM
+	{
+		public int Id { get; set; }
+
+		[AppRequired]
+		[AppStringLength(VM.CategoryNewsVM.MIN_LENGTH, DB.AppNewsCategory.MAX_LENGTH)]
+		public string Title { get; set; }
+		[AppMaxLength(DB.AppNewsCategory.MAX_LENGTH)]
+		public string? Content { get; set; }
+		public string? CoverImgPath { get; set; }
+		public IFormFile? ImgPath { get; set; }
+		public DateTime? UpdatedDate { get; set; }
+	}
+}
---
+++ App.Web/Areas/Admin/ViewModels/News/AddOrUpdateNewsVM.cs
@@ -0,0 +1,22 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+
+namespace App.Web.Areas.Admin.ViewModels.News
+{
+    public class AddOrUpdateNewsVM : ListItemBaseVM
+    {
+        [AppRequired]
+        [AppMaxLength(DB.AppNews.MAX_LENGTH)]
+        public string Title { get; set; }
+        [AppMaxLength(DB.AppNews.MAX_LENGTH)]
+        public string? Summary { get; set; }
+        public string? Content { get; set; }
+        [AppRequired]
+        public int CategoryId { get; set; }
+        [AppRequired]
+        public string? CoverImgPath { get; set; }
+        public string? CoverImgThumbnailPath { get; set; }
+        public string? StampPath { get; set; }
+        public bool SendAllEmail { get; set; }
+    }
+}
