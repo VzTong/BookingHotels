c3aaeb466367edc8c15860d2fa972f0682b8b42a
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-17 10:47:22 +0700

    Update ListRoleViewComponent.cs

--- App.Web/Areas/Admin/Components/ListRole/ListRoleViewComponent.cs
+++ App.Web/Areas/Admin/Components/ListRole/ListRoleViewComponent.cs
@@ -2,6 +2,8 @@
 using App.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+using System.Security.Claims;
 
 namespace App.Web.Areas.Admin.Components.ListRole
 {
@@ -14,11 +16,26 @@ namespace App.Web.Areas.Admin.Components.ListRole
 		}
 		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
 		{
+			int? brandId = GetCurrentUserBranchId();
 			var data = await repository
-					.GetAll<AppRole>(where: r => excludeIds == null || excludeIds.Any() && !excludeIds.Contains(r.Id))
-					.ToListAsync();
+			   .GetAll<AppRole>(where: r =>
+				   (r.Id != 1) &&
+				   (brandId == null || repository.DbContext.AppUsers
+					   .Any(ur => ur.AppRoleId == r.Id && ur.Branch.Id == brandId)) &&
+				   (excludeIds == null || !excludeIds.Any() || !excludeIds.Contains(r.Id)))
+			   .ToListAsync();
+
 			ViewBag.SelectedId = seletetedId;
 			return View(data);
+		}
+
+		protected int CurrentUserId { get => Convert.ToInt32(HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)); }
+		protected string CurrentUsername { get => HttpContext.User.Identity.Name; }
+
+		protected int? GetCurrentUserBranchId()
+		{
+			var user = repository.DbContext.AppUsers.AsNoTracking().FirstOrDefault(u => u.Id == CurrentUserId);
+			return user?.BranchId;
 		}
 	}
 }
