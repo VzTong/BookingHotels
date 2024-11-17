41eb923100cf7a704e9ce4ae4daa55dcef5904d5
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-17 20:14:24 +0700

    Update ListBranchViewComponent.cs

--- App.Web/Areas/Admin/Components/ListBranch/ListBranchViewComponent.cs
+++ App.Web/Areas/Admin/Components/ListBranch/ListBranchViewComponent.cs
@@ -2,6 +2,7 @@
 using App.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace App.Web.Areas.Admin.Components.ListBranchHotel
 {
@@ -14,11 +15,24 @@ namespace App.Web.Areas.Admin.Components.ListBranchHotel
 		}
 		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
 		{
+			int? brandId = GetCurrentUserBranchId();
 			var data = await repository
-					.GetAll<AppBranchHotel>(where: r => excludeIds == null || excludeIds.Any() && !excludeIds.Contains(r.Id))
-					.ToListAsync();
+			   .GetAll<AppBranchHotel>(where: r =>
+				   (brandId == null || r.Id == brandId) &&
+				   (excludeIds == null || !excludeIds.Any() || !excludeIds.Contains(r.Id)))
+			   .ToListAsync();
+
 			ViewBag.SelectedId = seletetedId;
 			return View(data);
+		}
+
+		protected int CurrentUserId { get => Convert.ToInt32(HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)); }
+		protected string CurrentUsername { get => HttpContext.User.Identity.Name; }
+
+		protected int? GetCurrentUserBranchId()
+		{
+			var user = repository.DbContext.AppUsers.AsNoTracking().FirstOrDefault(u => u.Id == CurrentUserId);
+			return user?.BranchId;
 		}
 	}
 }
