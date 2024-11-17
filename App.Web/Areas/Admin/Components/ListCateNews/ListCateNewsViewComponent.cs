8c1dd3dd8fd6d77aadd5220b3e8ceb3a2832c266
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-17 10:51:53 +0700

    Create ListCateNews components

---
+++ App.Web/Areas/Admin/Components/ListCateNews/ListCateNewsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using App.Data.Entities.News;
+using App.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Areas.Admin.Components.ListCategoryNews
+{
+	public class ListCateNewsViewComponent : ViewComponent
+	{
+		readonly GenericRepository repository;
+		public ListCateNewsViewComponent(GenericRepository _db)
+		{
+			repository = _db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
+		{
+			var data = await repository
+					.GetAll<AppNewsCategory>(where: r => excludeIds == null || excludeIds.Any() && !excludeIds.Contains(r.Id))
+					.ToListAsync();
+			ViewBag.SelectedId = seletetedId;
+			return View(data);
+		}
+	}
+}
