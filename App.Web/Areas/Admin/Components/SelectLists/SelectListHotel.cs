9d1e2e48914130e1e51148736f8fbca7c4143b34
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-17 10:47:42 +0700

    Update SelectListHotel.cs

--- App.Web/Areas/Admin/Components/SelectLists/SelectListHotel.cs
+++ App.Web/Areas/Admin/Components/SelectLists/SelectListHotel.cs
@@ -3,6 +3,7 @@ using App.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace App.Web.Areas.Admin.Components.SelectLists
 {
@@ -16,26 +17,43 @@ namespace App.Web.Areas.Admin.Components.SelectLists
 
         public async Task<IViewComponentResult> InvokeAsync(List<int>? selectedValue, string _for, bool allowNull)
         {
-            var hotelList = await repository
-                .GetAll<AppHotel>()
-                .OrderByDescending(s => s.CreatedDate)
-                .ToListAsync();
+			int? branchId = GetCurrentUserBranchId();
 
-            if (hotelList == null || !hotelList.Any())
-            {
+			IQueryable<AppHotel> query = repository.GetAll<AppHotel>();
+
+			if (branchId != null)
+			{
+				query = query.Where(h => h.BranchHotels.Any(bh => bh.Id == branchId));
+			}
+
+			var hotelList = await query
+				.OrderByDescending(s => s.CreatedDate)
+				.ToListAsync();
+
+			if (hotelList == null || !hotelList.Any())
+			{
 				// Xử lý khi proCate là null hoặc không có phần tử nào
 				throw new Exception("Thuộc khách sạn không được để rỗng");
-            }
+			}
 
 			var listHotel = new SelectList(hotelList, "Id", "Name", -1);
 			if (selectedValue != null)
-            {
+			{
 				listHotel = new SelectList(hotelList, "Id", "Name", selectedValue);
 			};
 			ViewBag.HotelList = listHotel;
-            ViewBag.For = _for;
-            ViewBag.AllowNull = allowNull;
+			ViewBag.For = _for;
+			ViewBag.AllowNull = allowNull;
 			return View();
-        }
-    }
+		}
+
+		protected int CurrentUserId { get => Convert.ToInt32(HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)); }
+		protected string CurrentUsername { get => HttpContext.User.Identity.Name; }
+
+		protected int? GetCurrentUserBranchId()
+		{
+			var user = repository.DbContext.AppUsers.AsNoTracking().FirstOrDefault(u => u.Id == CurrentUserId);
+			return user?.BranchId;
+		}
+	}
 }
