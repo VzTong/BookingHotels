0da025de8af97a45d004f5f50df783c03bd6b613
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-20 23:02:18 +0700

    Create RType js

---
+++ App.Web/wwwroot/js/Admin/RType/rType-modal.js
@@ -0,0 +1,15 @@
+﻿function UpdateRType(id, roomTypeName, peopleStay, bringPet, description) {
+    // Set values in the update modal
+    $('#updateRType #idRType').val(id);
+    $('#updateRType #rTypeName-field').val(roomTypeName);
+    $('#updateRType #rTypePeopleStay-field').val(peopleStay);
+    if (bringPet == 'True') {
+        $('#updateRType #rTypeBringPet-field').prop('checked', bringPet);
+    }
+    $('#updateRType #rTypeDecs-field').val(description);
+}
+
+function setDeleteId(id) {
+	const deleteConfirmBtn = document.getElementById('deleteConfirmBtn');
+	deleteConfirmBtn.href = `/Admin/AppRType/Delete/${id}`;
+}
---
+++ App.Web/wwwroot/js/Admin/RType/rType-validate.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    // Room type name validation
+    $('#rTypeName-field').on('input', function () {
+        var name = $(this);
+        var value = name.val().trim();
+        var isValid = value.length >= 1 && value.length <= 100;
+
+        if (isValid) {
+            name[0].setCustomValidity('');
+            name.removeClass('is-invalid').addClass('is-valid');
+        } else {
+            name[0].setCustomValidity('Tên loại phòng không hợp lệ');
+            name.removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+
+    // PeopleStay validation
+    $('#rTypePeopleStay-field').on('input', function () {
+        var name = $(this);
+        var value = name.val().trim();
+        var isValid = value.length >= 1 && value.length <= 2;
+
+        if (isValid) {
+            name[0].setCustomValidity('');
+            name.removeClass('is-invalid').addClass('is-valid');
+        } else {
+            name[0].setCustomValidity('Số người ở không hợp lệ');
+            name.removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+});
