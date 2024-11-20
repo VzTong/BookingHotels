0964187674a5066beefd58bb6cf7693322c479f1
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-20 23:01:51 +0700

    Create role js

---
+++ App.Web/wwwroot/js/Admin/Role/role-validate.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    // Role name validation
+    $('#roleName-field').on('input', function () {
+        var name = $(this);
+        var value = name.val().trim();
+        var isValid = value.length >= 1 && value.length <= 50;
+
+        if (isValid) {
+            name[0].setCustomValidity('');
+            name.removeClass('is-invalid').addClass('is-valid');
+        } else {
+            name[0].setCustomValidity('Tên quyền không hợp lệ');
+            name.removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+
+    // Role desc validation
+    $('#roleDesc-field').on('input', function () {
+        var name = $(this);
+        var value = name.val().trim();
+        var isValid = value.length >= 1 && value.length <= 100;
+
+        if (isValid) {
+            name[0].setCustomValidity('');
+            name.removeClass('is-invalid').addClass('is-valid');
+        } else {
+            name[0].setCustomValidity('Mô tả không hợp lệ');
+            name.removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+});
