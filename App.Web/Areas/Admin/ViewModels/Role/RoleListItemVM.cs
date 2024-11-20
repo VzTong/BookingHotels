20ad4a8e8df55658398c9484dd4a761907b10bc8
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-20 22:47:35 +0700

    Create RoleListItemVM.cs

---
+++ App.Web/Areas/Admin/ViewModels/Role/RoleListItemVM.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Role
+{
+	public class RoleListItemVM : ListItemBaseVM
+	{
+		public string Name { get; set; }
+		public string Desc { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public string? BranchName { get; set; }
+	}
+}
