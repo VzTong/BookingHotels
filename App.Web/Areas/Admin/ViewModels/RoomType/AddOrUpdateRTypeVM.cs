fd3aaa2959af55609a45277b3eda7f537886091a
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-20 22:49:00 +0700

    Create VM RoomType

---
+++ App.Web/Areas/Admin/ViewModels/RoomType/AddOrUpdateRTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using App.Share.Attributes;
+using App.Share.Consts;
+using App.Web.WebConfig.Consts;
+
+namespace App.Web.Areas.Admin.ViewModels.RoomType
+{
+	public class AddOrUpdateRTypeVM
+	{
+		public int? Id { get; set; }
+
+		[AppRequired]
+		[AppStringLength(VM.ProductCategoryVM.MIN_LENGTH, DB.AppRoomType.NAME_LENGTH)]
+		public string RoomTypeName { get; set; }
+		public int PeopleStay { get; set; }
+		public string? Description { get; set; }
+		public bool BringPet { get; set; }
+	}
+}
---
+++ App.Web/Areas/Admin/ViewModels/RoomType/RTypeListItemVM.cs
@@ -0,0 +1,15 @@
+﻿using App.Data.Entities.Room;
+
+namespace App.Web.Areas.Admin.ViewModels.RoomType
+{
+	public class RTypeListItemVM : ListItemBaseVM
+	{
+		public string RoomTypeName { get; set; }
+		public int PeopleStay { get; set; }
+		public string? Description { get; set; }
+		public bool BringPet { get; set; }
+		public int? RoomId { get; set; }
+		public List<string> RoomName { get; set; }
+		public ICollection<AppRoom> AppRooms { get; set; }
+	}
+}
