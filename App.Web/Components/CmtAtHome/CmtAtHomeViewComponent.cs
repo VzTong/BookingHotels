11ea2539ac8745a4e768deb0a4d548274580ae50
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-21 13:36:52 +0700

    Create Components CmtAtHome

---
+++ App.Web/Components/CmtAtHome/CmtAtHomeViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using App.Data.Entities.service;
+using App.Data.Repositories;
+using App.Web.ViewModels.Cmt;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace App.Web.Components.CmtAtHome
+{
+	public class CmtAtHomeViewComponent : ViewComponent
+	{
+		private readonly GenericRepository _repo;
+
+		public CmtAtHomeViewComponent(GenericRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var data = await _repo
+				   .GetAll<AppComment>(x => x.DeletedDate == null)
+				   .OrderBy(x => x.DisplayOrder)
+				   .Select(x => new CmtVM
+				   {
+					   Id = x.Id,
+					   Description = x.Description,
+					   UserName = x.User.FullName,
+					   UserAvt = x.User.Avatar,
+				   })
+				   .ToListAsync();
+			return View(data);
+		}
+	}
+}
