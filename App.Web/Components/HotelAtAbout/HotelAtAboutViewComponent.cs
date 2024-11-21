39fe4dc8e95c8b117377c6ebae32aa0e861e7acf
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-21 13:37:12 +0700

    Create Components HotelAtAbout

---
+++ App.Web/Components/HotelAtAbout/HotelAtAboutViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using App.Data.Entities.Hotel;
+using App.Data.Repositories;
+using App.Web.ViewModels.Hotel;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace App.Web.Components.HotelAtAbout
+{
+	public class HotelAtAboutViewComponent : ViewComponent
+	{
+		private readonly GenericRepository _repo;
+
+		public HotelAtAboutViewComponent(GenericRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var data = await _repo
+				   .GetAll<AppHotel>(x => x.DeletedDate == null && x.IsActive == true)
+				   .OrderBy(x => x.DisplayOrder)
+				   .Select(x => new HotelVM
+				   {
+					   Id = x.Id,
+					   Name = x.Name,
+					   Slug = x.Slug,
+					   Description = x.Description,
+					   ImgBanner = x.ImgBanner
+				   })
+				   .Take(8)
+				   .ToListAsync();
+			return View(data);
+		}
+	}
+}
