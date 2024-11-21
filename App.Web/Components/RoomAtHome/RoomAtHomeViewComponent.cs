51241a2929503a02a29a7fddaf11eb96d5c23e09
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-21 13:37:51 +0700

    Create Components RoomAtHome

---
+++ App.Web/Components/RoomAtHome/RoomAtHomeViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using App.Data.Entities.Room;
+using App.Data.Repositories;
+using App.Web.ViewModels.Room;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace App.Web.Components.RoomAtHome
+{
+	public class RoomAtHomeViewComponent : ViewComponent
+	{
+		private readonly GenericRepository _repo;
+
+		public RoomAtHomeViewComponent(GenericRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var data = await _repo
+				   .GetAll<AppRoom>(x => x.DeletedDate == null && x.IsActive == true)
+				   .OrderBy(x => x.DisplayOrder)
+				   .Select(x => new RoomVM
+				   {
+					   Id = x.Id,
+					   RoomName = x.RoomName,
+					   Slug = x.Slug,
+					   Price = x.Price,
+					   DiscountPrice = x.DiscountPrice,
+					   HotelName = x.Branch.Hotel.Name,
+					   BranchDesc = x.Branch.Description,
+					   ImagePath = x.ImgRooms.FirstOrDefault().ImgSrc
+				   })
+				   .ToListAsync();
+			return View(data);
+		}
+	}
+}
