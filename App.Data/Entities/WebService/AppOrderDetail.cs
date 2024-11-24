0fc85b300f9c17df9b20ca8333fb9375f2b98056
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-24 21:11:04 +0700

    Update AppOrderDetail.cs

--- App.Data/Entities/WebService/AppOrderDetail.cs
+++ App.Data/Entities/WebService/AppOrderDetail.cs
@@ -1,23 +1,21 @@
 ﻿using App.Data.Entities.Base;
-using App.Data.Entities.Hotel;
 using App.Data.Entities.Room;
+using App.Data.Entities.User;
 
 namespace App.Data.Entities.service
 {
-    public class AppOrderDetail : AppEntityBase
+	public class AppOrderDetail : AppEntityBase
     {
-        public DateTime InvoiceDate { get; set; }
-        public string RoomName { get; set; }
-        public int RoomNumber { get; set; }
-        public string RoomType { get; set; }
-        public double Price { get; set; }
-        public double? DiscountPrice { get; set; }
-        public string FullNameUser { get; set; }
-        public int QuantityRoom { get; set; }
-        public int RoomId { get; set; }
-        public int OrderId { get; set; }
+		public DateTime CheckInTime_Expected { get; set; }
+		public DateTime CheckOutTime_Expected { get; set; }
+		public DateTime? CheckInTime { get; set; }
+		public DateTime? CheckOutTime { get; set; }
+		public int? TimeStay { get; set; }
+        public int? RoomId { get; set; }
+        public int? OrderId { get; set; }
 
-        public AppRoom Room { get; set; }
+		public AppRoom Room { get; set; }
         public AppOrder Order { get; set; }
+        public AppUser Employee { get; set; }
     }
 }
