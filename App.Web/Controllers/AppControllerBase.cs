8b3dc5770d94949d83fd5467eabc7ae1e7e22f9a
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-24 15:26:02 +0700

    Update AppControllerBase.cs

--- App.Web/Controllers/AppControllerBase.cs
+++ App.Web/Controllers/AppControllerBase.cs
@@ -12,6 +12,12 @@ namespace App.Web.Controllers
 	{
 		protected readonly IMapper _mapper;
 		protected readonly GenericRepository _repository;
+		protected const int DEFAULT_PAGE_SIZE = 20;
+		protected const string EXCEPTION_ERR_MESG = "Đã xảy ra lỗi trong quá trình xử lý dữ liệu (500)";
+		protected const string MODEL_STATE_INVALID_MESG = "Dữ liệu không hợp lệ, vui lòng kiểm tra lại";
+		protected const string PAGE_NOT_FOUND_MESG = "Không tìm thấy trang";
+		protected readonly string DefaultImagePath = "upload/img_avt/astronaut.png";// Đường dẫn đến ảnh mặc định
+
 		protected int CurrentUserId { get => Convert.ToInt32(HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)); }
 		protected string CurrentUsername { get => HttpContext.User.Identity.Name; }
 		public AppControllerBase(IMapper mapper, GenericRepository repository)
@@ -19,6 +25,25 @@ namespace App.Web.Controllers
 			_mapper = mapper;
 			_repository = repository;
 		}
+
+		/// <summary>
+		/// Gán thông báo lỗi để hiển thị lên view
+		/// </summary>
+		/// <param name="mesg">Thông báo lỗi</param>
+		/// <param name="modelStateIsInvalid">Đặt là true khi lỗi validate dữ liệu</param>
+		protected void SetErrorMesg(string mesg, bool modelStateIsInvalid = false)
+		{
+			TempData["Err"] = mesg;
+			if (modelStateIsInvalid)
+			{
+				// hiển thị tin nhắn lỗi ở file log
+				var invalidMesg = string.Join("\n", ModelState.Values
+												.SelectMany(v => v.Errors)
+												.Select(e => e.ErrorMessage));
+			}
+		}
+
+		protected void SetSuccessMesg(string mesg) => TempData["Success"] = mesg;
 
 		protected byte[] HashHMACSHA512WithKey(string pwd, byte[] key)
 		{
