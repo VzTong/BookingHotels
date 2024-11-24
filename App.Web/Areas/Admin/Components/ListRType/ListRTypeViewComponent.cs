1702a340ea9d2369d325b43a561949ce4574f30d
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-24 15:23:42 +0700

    Create component List Room type

---
+++ App.Web/Areas/Admin/Components/ListRType/ListRTypeViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using App.Data.Entities.Room;
+using App.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace App.Web.Areas.Admin.Components.ListRType
+{
+	public class ListRTypeViewComponent : ViewComponent
+	{
+		readonly GenericRepository repository;
+		public ListRTypeViewComponent(GenericRepository _db)
+		{
+			repository = _db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync(int? seletetedId, IEnumerable<int> excludeIds)
+		{
+			var data = await repository
+			   .GetAll<AppRoomType>(where: r => (excludeIds == null || !excludeIds.Any() || !excludeIds.Contains(r.Id)))
+			   .ToListAsync();
+
+			ViewBag.SelectedId = seletetedId;
+			return View(data);
+		}
+	}
+}
