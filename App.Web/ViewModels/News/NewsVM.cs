37f48a07ce895dbb54c4495b1f4cb51cd75ca888
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-24 15:26:57 +0700

    Create NewsVM.cs

---
+++ App.Web/ViewModels/News/NewsVM.cs
@@ -0,0 +1,18 @@
+﻿namespace App.Web.ViewModels.News
+{
+	public class NewsVM
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public long Views { get; set; }
+		public float Votes { get; set; }
+        public string? Summary { get; set; }
+        public string? Content { get; set; }
+        public string? CoverImgPath { get; set; }
+		public string? CoverImgThumbnailPath { get; set; }
+		public string? StampPath { get; set; }
+		public string CreatedByName { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public string? CategoryName { get; set; }
+	}
+}
