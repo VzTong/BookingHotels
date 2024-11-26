b57d258e531b7752050261b61a39a61779221936
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-26 17:08:40 +0700

    Create OrderVM

---
+++ App.Web/Areas/Admin/ViewModels/Order/ListItemOrderDetailVM.cs
@@ -0,0 +1,18 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Order
+{
+	public class ListItemOrderDetailVM : ListItemBaseVM
+	{
+		public string? ImagePath { get; set; }
+		public int? OrderId { get; set; }
+		public int? RoomId { get; set; }
+		public string RoomName { get; set; }
+		public decimal RoomPrice { get; set; }
+		public int QuantityRoom { get; set; }
+
+		public DateTime CheckInTime_Expected { get; set; }
+		public DateTime CheckOutTime_Expected { get; set; }
+		public DateTime? CheckInTime { get; set; }
+		public DateTime? CheckOutTime { get; set; }
+		public int? TimeStay { get; set; }
+	}
+}
---
+++ App.Web/Areas/Admin/ViewModels/Order/ListItemOrderVM.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Order
+{
+	public class ListItemOrderVM : ListItemBaseVM
+	{
+		public string CusName { get; set; }
+		public string CusPhone { get; set; }
+		public string CusEmail { get; set; }
+		public string DeliveryAddress { get; set; }
+		public string CusNote { get; set; }
+		public decimal TotalPrice { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public ICollection<ListItemOrderDetailVM> AppOrderDetails { get; set; }
+	}
+}
