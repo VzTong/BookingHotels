f9b8dbb33a476eec48336a9e1e513e2c06bfe895
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-28 08:02:55 +0700

    Update AppComment.cs

--- App.Data/Entities/WebService/AppComment.cs
+++ App.Data/Entities/WebService/AppComment.cs
@@ -6,6 +6,7 @@ namespace App.Data.Entities.service
 	public class AppComment : AppEntityBase
 	{
 		public string Description { get; set; }
+		public int Rating { get; set; }
 
 		public AppUser User { get; set; }
 	}
