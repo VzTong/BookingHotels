9b81db842f78b1ff2c2fdfdc11fcd35526fec5e9
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-28 17:42:39 +0700

    Create CmtListItemVM.cs

---
+++ App.Web/Areas/Admin/ViewModels/Comment/CmtListItemVM.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Web.Areas.Admin.ViewModels.Comment
+{
+	public class CmtListItemVM : ListItemBaseVM
+	{
+		public string UserName { get; set; }
+		public string Description { get; set; }
+		public int Rating { get; set; }
+	}
+}
