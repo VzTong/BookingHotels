0c4191f38b84be0ed0d8d22c939dcc6ec7554abb
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-28 08:04:52 +0700

    Create SendCmtVM.cs

---
+++ App.Web/ViewModels/Cmt/SendCmtVM.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Web.ViewModels.Cmt
+{
+	public class SendCmtVM
+	{
+		public int? Id { get; set; }
+		public string Description { get; set; }
+		public int Rating { get; set; }
+	}
+}
