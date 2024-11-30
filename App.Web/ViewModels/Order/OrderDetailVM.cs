97dc023a11661aa74641ec31e2695c9853b2add6
Vz <119030528+VzTong@users.noreply.github.com>
2024-12-01 00:07:03 +0700

    Create Order & Detail VM

---
+++ App.Web/ViewModels/Order/OrderDetailVM.cs
@@ -0,0 +1,27 @@
+﻿using App.Data.Entities.Room;
+using App.Web.ViewModels.Room;
+
+namespace App.Web.ViewModels.Order
+{
+	public class OrderDetailVM
+	{
+		public int Id { get; set; }
+		public int? OrderId { get; set; }
+		public string RoomName { get; set; }
+		public string RoomStatus { get; set; }
+		public decimal? DiscountPrice { get; set; }
+		public DateTime? DiscountFrom { get; set; }
+		public DateTime? DiscountTo { get; set; }
+
+		public DateTime CheckInTime_Expected { get; set; }
+		public DateTime CheckOutTime_Expected { get; set; }
+		public DateTime? CheckInTime { get; set; }
+		public DateTime? CheckOutTime { get; set; }
+		public int? TimeStay { get; set; }
+		public decimal? TotalPrice { get; set; }
+
+		public int? UpdatedBy { get; set; }
+		public DateTime? UpdatedDate { get; set; }
+		public DateTime? DeletedDate { get; set; }
+	}
+}
---
+++ App.Web/ViewModels/Order/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿namespace App.Web.ViewModels.Order
+{
+	public class OrderVM
+	{
+		public int Id { get; set; }
+		public Decimal? TotalAmount { get; set; }
+		public int QuantityRoom { get; set; }
+		public string Status { get; set; }
+		public List<OrderDetailVM> AppOrderDetails { get; set; }
+	}
+}
