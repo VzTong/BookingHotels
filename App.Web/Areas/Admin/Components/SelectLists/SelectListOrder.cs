1afd799d3361de762442211e9649bab7fd892934
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-30 20:13:38 +0700

    Create SelectListOrder.cs

---
+++ App.Web/Areas/Admin/Components/SelectLists/SelectListOrder.cs
@@ -0,0 +1,25 @@
+﻿using App.Data.Entities.service;
+using App.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Areas.Admin.Components.SelectLists
+{
+	public class SelectListOrder : ViewComponent
+	{
+		readonly GenericRepository repository;
+		public SelectListOrder(GenericRepository _db)
+		{
+			repository = _db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var data = await repository.GetAll<AppOrder>()
+				.Include(s => s.OrderDetails)
+				.ThenInclude(s => s.Room)
+				.OrderBy(x => x.DisplayOrder)
+				.ToListAsync();
+			return View(data);
+		}
+	}
+}
