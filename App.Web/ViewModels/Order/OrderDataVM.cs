aa9b1c824d091dd8cc41cfca1bdf07f1bae1c3aa
Vz <119030528+VzTong@users.noreply.github.com>
2024-11-30 20:14:55 +0700

    Update OrderDataVM.cs

--- App.Web/ViewModels/Order/OrderDataVM.cs
+++ App.Web/ViewModels/Order/OrderDataVM.cs
@@ -6,8 +6,12 @@
 		public string CusPhone { get; set; }
 		public string CusEmail { get; set; }
 		public string? CusNote { get; set; }
+		public int? CusCitizenId { get; set; }
+		public string? CusPassport { get; set; }
+		public string? CusPermanent { get; set; }
 		public decimal? Total { get; set; }
 		public string? Status { get; set; }
+		public int QuantityRoom { get; set; }
 		public string? ImagePath { get; set; }
 
 		public DateTime CheckInTime_Expected { get; set; }
