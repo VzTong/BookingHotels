14cad196ff1e5830dc9acb6963f4def3b40cb5d2
Vz <119030528+VzTong@users.noreply.github.com>
2024-12-13 08:38:50 +0700

    Create SearchRoomClientVM.cs

---
+++ App.Web/ViewModels/Room/SearchRoomClientVM.cs
@@ -0,0 +1,11 @@
+﻿namespace App.Web.ViewModels.Room
+{
+	public class SearchRoomClientVM
+	{
+		public string? Addr { get; set; }
+		public int? Adults { get; set; }
+
+		public DateTime? CheckInTime_Expected { get; set; }
+		public DateTime? CheckOutTime_Expected { get; set; }
+	}
+}
