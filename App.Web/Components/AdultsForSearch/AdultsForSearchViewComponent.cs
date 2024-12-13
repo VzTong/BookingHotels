ecf66cdd05820ea119e3009d37a322f9f7dd37d4
Vz <119030528+VzTong@users.noreply.github.com>
2024-12-13 08:38:07 +0700

    Create AdultsForSearchViewComponent.cs

---
+++ App.Web/Components/AdultsForSearch/AdultsForSearchViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using App.Data.Entities.Room;
+using App.Data.Repositories;
+using App.Web.ViewModels.Cmt;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using X.PagedList;
+
+namespace App.Web.Components.AdultsForSearch
+{
+	public class AdultsForSearchViewComponent : ViewComponent
+	{
+		private readonly GenericRepository _repo;
+
+		public AdultsForSearchViewComponent(GenericRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var data = await _repo
+				   .GetAll<AppRoomType>(x => x.DeletedDate == null)
+				   .OrderBy(x => x.DisplayOrder)
+				   .Distinct()
+				   .ToListAsync();
+			return View(data);
+		}
+	}
+}
